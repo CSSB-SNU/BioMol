34c0cd11d4fd5c43d517e88dce55c58c7e3ef913
SanggeunParrk <psk6950@snu.ac.kr>
2025-04-28 11:56:18 +0900

    fix get_edge_score.py

--- statistics/get_edge_score.py
+++ statistics/get_edge_score.py
@@ -1,6 +1,4 @@
 import pickle
-import os
-import networkx as nx
 import re
 from joblib import Parallel, delayed
 import matplotlib.pyplot as plt
@@ -8,93 +6,62 @@ import matplotlib.pyplot as plt
 # Regex pattern to capture the three groups inside the tuple
 pattern = re.compile(r"\('([^']+)', '([^']+)', '([^']+)'\)")
 
-
-def format_tuple(s):
-    match = pattern.search(s)
-    if match:
-        # Combine the captured groups with underscores
-        return f"{match.group(1)}_{match.group(2)}_{match.group(3)}"
-    return None
+def get_unique_graphs(unique_graph_path):
+    with open(unique_graph_path, "rb") as f:
+        unique_graphs = pickle.load(f)
+    return unique_graphs
 
 
-def parse_graph(file_path: str) -> list[nx.Graph]:
-    graph_ID = file_path.split("/")[-1].split(".")[0]
-    G_dict = {}
-    G = nx.Graph()  # Use nx.DiGraph() for directed graphs
-    ID = None
-    with open(file_path) as f:
-        for line in f:
-            parts = line.strip().split()
-            if not parts:
-                continue
-            if parts[0] == "t":  # New graph line: "t ID"
-                if G.number_of_nodes() > 0:
-                    G_dict[ID] = G
-                    G = nx.Graph()
-                parts = line.strip().split("#")
-                ID = format_tuple(parts[1])
-                ID = f"{graph_ID}_{ID}"
-            elif parts[0] == "v":  # Vertex line: "v id label"
-                node_id = int(parts[1])
-                label = parts[2]
-                G.add_node(node_id, label=label)
-            elif parts[0] == "e":  # Edge line: "e src tgt"
-                src, tgt = int(parts[1]), int(parts[2])
-                G.add_edge(src, tgt)
-        if G.number_of_nodes() > 0:
-            G_dict[ID] = G
-    return G_dict
+def get_frequency(unique_graph_path, node_save_path, edge_save_path):
+    unique_graphs = get_unique_graphs(unique_graph_path)
 
 
-def get_frequency(graph_dir, node_save_path, edge_save_path, graph_save_path):
-    # 1. Collect all graph file paths.
-    graph_files = []
-    for root, _, files in os.walk(graph_dir):
-        for file in files:
-            if file.endswith(".graph"):
-                graph_files.append(os.path.join(root, file))
-
-    # 2. Parse graphs in parallel.
-    results = Parallel(n_jobs=-1)(
-        delayed(parse_graph)(graph_path) for graph_path in graph_files
-    )
-    graph_dict = {}
-    for d in results:
-        graph_dict.update(d)
-
-    pdb_ID_to_graph_num = {}
-    for k in graph_dict.keys():
-        pdb_ID = k.split("_")[0]
-        if pdb_ID not in pdb_ID_to_graph_num:
-            pdb_ID_to_graph_num[pdb_ID] = 1
-        else:
-            pdb_ID_to_graph_num[pdb_ID] += 1
-
+    node_score_dict = {}
     edge_score_dict = {}
-    for k, v in graph_dict.items():
-        graph_num = pdb_ID_to_graph_num[k.split("_")[0]]
-        edge_num = len(v.edges())
-        temp_label_data = []
-        for edge in v.edges():
-            src_label = v.nodes[edge[0]]["label"]
-            tgt_label = v.nodes[edge[1]]["label"]
-            edge_label = (src_label, tgt_label)
-            if edge_label in temp_label_data:
+    for graph_hash, graph in unique_graphs.items():
+        node_num = len(graph.nodes())
+        edge_num = len(graph.edges())
+        temp_node_label_data = []
+        temp_edge_label_data = []
+        for edge in graph.edges():
+            src_label = graph.nodes[edge[0]]["label"]
+            tgt_label = graph.nodes[edge[1]]["label"]
+            edge_label = tuple(sorted((src_label, tgt_label)))
+            if edge_label in temp_edge_label_data:
                 continue
-            edge_score = 1/ (edge_num*graph_num)
+            edge_score = 1/ (edge_num)
             if edge_label not in edge_score_dict:
                 edge_score_dict[edge_label] = edge_score
             else:
                 edge_score_dict[edge_label] += edge_score
-            temp_label_data.append(edge_label)
+            temp_edge_label_data.append(edge_label)
+
+        for node in graph.nodes():
+            node_label = graph.nodes[node]["label"]
+            if node_label in temp_node_label_data:
+                continue
+            node_score = 1 / (node_num)
+            if node_label not in node_score_dict:
+                node_score_dict[node_label] = node_score
+            else:
+                node_score_dict[node_label] += node_score
+            temp_node_label_data.append(node_label)
 
     # print max and min edge score and its ID
-    max_edge, max_score = max(edge_score_dict.items(), key=lambda kv: kv[1])
-    min_edge, min_score = min(edge_score_dict.items(), key=lambda kv: kv[1])
-    print(f"Max-scoring edge {max_edge!r} → {max_score:.6f}")
-    print(f"Min-scoring edge {min_edge!r} → {min_score:.6f}")
+    max_node, max_node_score = max(node_score_dict.items(), key=lambda kv: kv[1])
+    min_node, min_node_score = min(node_score_dict.items(), key=lambda kv: kv[1])
+    print(f"Max-scoring node {max_node!r} → {max_node_score:.6f}")
+    print(f"Min-scoring node {min_node!r} → {min_node_score:.6f}")
+
+    max_edge, max_edge_score = max(edge_score_dict.items(), key=lambda kv: kv[1])
+    min_edge, min_edge_score = min(edge_score_dict.items(), key=lambda kv: kv[1])
+    print(f"Max-scoring edge {max_edge!r} → {max_edge_score:.6f}")
+    print(f"Min-scoring edge {min_edge!r} → {min_edge_score:.6f}")
 
     # save node_freq, edge_score_dict
+    with open(node_save_path, "wb") as f:
+        pickle.dump(node_score_dict, f)
+
     with open(edge_save_path, "wb") as f:
         pickle.dump(edge_score_dict, f)
 
@@ -149,11 +116,10 @@ def get_frequency(graph_dir, node_save_path, edge_save_path, graph_save_path):
 
 
 if __name__ == "__main__":
-    graph_dir = "/data/psk6950/PDB_2024Oct21/protein_graph/"
-    node_save_path = "/data/psk6950/PDB_2024Oct21/protein_graph/node_freq.pkl"
-    edge_save_path = "/data/psk6950/PDB_2024Oct21/protein_graph/edge_score_dict.pkl"
-    graph_save_path = "/data/psk6950/PDB_2024Oct21/protein_graph/graph_freq.pkl"
-    node_path = "./node_freq.png"
-    edge_path = "./edge_score_dict.png"
-    get_frequency(graph_dir, node_save_path, edge_save_path, graph_save_path)
+    unique_graphs_path = "/public_data/BioMolDB_2024Oct21/protein_graph/unique_graphs.pkl"
+    node_save_path = "/public_data/BioMolDB_2024Oct21/protein_graph/node_freq.pkl"
+    edge_save_path = "/public_data/BioMolDB_2024Oct21/protein_graph/edge_score_dict.pkl"
+    get_frequency(unique_graphs_path, node_save_path, edge_save_path)
+    # node_path = "./node_freq.png"
+    # edge_path = "./edge_score_dict.png"
     # plot_histogram(node_save_path, edge_save_path, node_path, edge_path)
